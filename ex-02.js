a98a35c88a48c98908d6ea450d4e73c71f00f8ce
Salino3 <flaviobcn012@gmail.com>
2025-12-24 16:51:16 +0200

    save changes

---
+++ ex-02.js
@@ -0,0 +1,13 @@
+const scores = [80, 90, 100];
+let count = 0;
+const sum = scores.reduce((acc, score) => {
+  count++;
+  console.log("clog" + count, acc);
+  return acc + score;
+}, 1);
+console.log("SUM", sum);
+
+let max = [].reduce((best, n) => (n > best ? n : best), -Infinity);
+console.log("max".toUpperCase(), max);
+max = [-7].reduce((best, n) => (n > best ? n : best), -Infinity);
+console.log("max".toUpperCase(), max);
