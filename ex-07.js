4cb358fc064a0ee927353cfc464f9adaa09bb152
Salino3 <flaviobcn012@gmail.com>
2026-01-04 16:34:05 +0200

    save changes

---
+++ ex-07.js
@@ -0,0 +1,61 @@
+// Task 1
+// Filter even numbers.
+
+function filterEvenNumber(arr) {
+  return arr.filter((n) => n % 2 === 0);
+}
+
+console.log("Task 1: ", filterEvenNumber([10, 15, 20, 25, 30]));
+
+// Task 2
+// Return an array with only the names.
+
+function extraNames(users) {
+  return users.map((user) => user.name);
+}
+
+console.log(
+  "Task 2: ",
+  extraNames([
+    { id: 1, name: "Ana", age: 25 },
+    { id: 2, name: "Luis", age: 32 },
+    { id: 3, name: "Marta", age: 28 },
+  ])
+);
+
+// Task 3
+// Return how many properties the object has.
+
+function countPropertiesObject(obj) {
+  return Object.entries(obj).length;
+}
+
+console.log(
+  "Task 3: ",
+  countPropertiesObject({
+    name: "Carlos",
+    age: 35,
+    country: "Spain",
+  })
+);
+
+// Task 4
+// Return a new object where all values are UPPERCASE.
+
+function uppercaseObjectValues(obj) {
+  return Object.fromEntries(
+    Object.entries(obj).map(([itemKey, itemValue]) => [
+      itemKey,
+      itemValue.toUpperCase(),
+    ])
+  );
+}
+
+console.log(
+  "Task 4: ",
+  uppercaseObjectValues({
+    admin: "full access",
+    editor: "edit content",
+    viewer: "read only",
+  })
+);
