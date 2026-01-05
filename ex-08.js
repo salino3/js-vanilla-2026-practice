b62b1827e4c714314762bd3ba68458a5fe495639
Salino3 <flaviobcn012@gmail.com>
2026-01-05 17:46:14 +0200

    save changes

--- ex-08.js
+++ ex-08.js
@@ -20,21 +20,36 @@ function isPalindrome(s) {
 console.log("Task 1: ", isPalindrome("hello"));
 
 // Task 2
-// Given an array of numbers, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+// Given an array of numbers, move all 0's to the end of it while maintaining the relative
+// order of the non-zero elements.
 
 function moveZerosToEnds(arr) {
   let len = arr.filter((x) => x == 0).length;
-  console.log(len);
-  for (let x = 0; x < arr.length; x++) {
-    if (arr[x] == 0) {
-      arr[x] = null;
-    }
-  }
-  //
+
   for (let x = 0; x < len; x++) {
     arr.push(0);
   }
-  return arr.filter((n) => n != null);
+  return arr.filter((n, i) => n != 0 || i > arr.length - len - 1);
 }
 
-console.log("Task 2: ", moveZerosToEnds([0, 1, 0, 3, 12]));
+console.log("Task 2: ", moveZerosToEnds([0, 1, 0, 0, 0, 0, 3, 12, 0]));
+
+function moveZerosToEnds02(arr) {
+  let lastNonZeroFoundAt = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      console.log(arr[lastNonZeroFoundAt], arr[i]);
+      arr[lastNonZeroFoundAt] = arr[i];
+      lastNonZeroFoundAt++;
+    }
+  }
+
+  for (let i = lastNonZeroFoundAt; i < arr.length; i++) {
+    arr[i] = 0;
+  }
+
+  return arr;
+}
+
+console.log("Task 2 V2: ", moveZerosToEnds02([0, 1, 0, 0, 0, 0, 3, 12, 0]));
