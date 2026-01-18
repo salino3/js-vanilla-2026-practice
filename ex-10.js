4736d305e7d45b3b5ada0d7717219f6db45b2aa4
Salino3 <flaviobcn012@gmail.com>
2026-01-18 16:03:17 +0200

    save changes

--- ex-10.js
+++ ex-10.js
@@ -34,3 +34,36 @@ console.log(
     { id: 2, name: "Mouse", price: 25 },
   ]),
 );
+
+// Task 2
+// Create a function called organizeGuests that takes an array of strings and returns a new array that:
+// Normalizes the names: Converts all names to a standard format (e.g., all lowercase or "Title Case").
+// Removes duplicates: Ensures each name appears only once, regardless of how it was originally capitalized.
+// Sorts alphabetically: From A to Z but Marcos the birthday boy goes first
+
+function organizeGuests(rawGuests) {
+  return rawGuests
+    .map((name) => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase())
+    .filter((str, index, arr) => arr.indexOf(str) === index)
+    .sort((a, b) => {
+      // Marco the birthday boy
+      if (a == "Marcos") {
+        return -1;
+      }
+      a.localeCompare(b);
+    });
+}
+
+console.log(
+  "Task 2: ",
+  organizeGuests([
+    "lucía",
+    "Marcos",
+    "ANA",
+    "lucía",
+    "pedro",
+    "marcos",
+    "Zoe",
+    "ana",
+  ]),
+);
