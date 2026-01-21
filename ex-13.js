c765ae77a433632d856a31a1f1824cf353e1cc4a
Salino3 <flaviobcn012@gmail.com>
2026-01-21 19:14:38 +0200

    save changes

--- ex-13.js
+++ ex-13.js
@@ -1,8 +1,10 @@
 class SmartThermostat {
   // 1. Storage (Private)
-  #internalValue;
+  #internalValue = "[Unsafe temperature]"; // default value
 
-  constructor(startingTemp) {
+  constructor(
+    startingTemp = 0, // in case you don't insert value
+  ) {
     // 2. We use the SETTER name here to trigger the check
     this.validateAndSet = startingTemp;
   }
@@ -16,7 +18,7 @@ class SmartThermostat {
   set validateAndSet(newDegrees) {
     if (newDegrees < 0 || newDegrees > 40) {
       console.error(`Alert: ${newDegrees} is an unsafe temperature!`);
-      return;
+      return this.validateAndSet;
     }
     this.#internalValue = newDegrees;
   }
@@ -28,11 +30,17 @@ class SmartThermostat {
 }
 
 // --- TESTING ---
-const livingRoom = new SmartThermostat(22); // Works!
+const bedRoom = new SmartThermostat();
+console.log("clog1", bedRoom.viewTemperature); // Output: "undefined°C"
+
+const livingRoom = new SmartThermostat(22);
 const kitchen = new SmartThermostat(99); // Logs: Alert: 99 is an unsafe temperature!
 
-console.log(livingRoom.viewTemperature); // Output: "22°C"
-console.log(livingRoom.checkCurrentStatus()); // Output: "The device is currently set to 22°C"
+console.log("clog2", kitchen.viewTemperature); // Output: "[Unsafe temperature]°C"
+console.log("clog3", kitchen.checkCurrentStatus()); // "The device is currently set to [Unsafe temperature]°C"
+console.log("clog4", livingRoom.viewTemperature); // Output: "22°C"
+console.log("clog5", livingRoom.checkCurrentStatus()); // Output: "The device is currently set to 22°C"
 
 // Trying to bypass the gatekeeper:
 livingRoom.validateAndSet = -10; // Logs: Alert: -10 is an unsafe temperature!
+console.log("clog6", livingRoom.checkCurrentStatus()); // Output: "22°C"
