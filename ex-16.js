33d99ad57b8634e15104d835841355dbf3b88d73
Salino3 <flaviobcn012@gmail.com>
2026-03-30 14:56:29 +0200

    exercise for reaact immutability

--- ex-16.js
+++ ex-16.js
@@ -24,3 +24,33 @@ function TotalAvailablePrice(cart) {
 }
 
 console.log("Task 1 ", TotalAvailablePrice(cart));
+
+//
+const products = [
+  { name: "Apple", category: "Fruit", price: 0.5 },
+  { name: "Banana", category: "Fruit", price: 0.3 },
+  { name: "Steak", category: "Meat", price: 10.0 },
+  { name: "Chicken", category: "Meat", price: 7.0 },
+  { name: "Carrot", category: "Vegetable", price: 0.2 },
+];
+
+// Write a function called groupByCategory that:
+
+// Uses .reduce() to return a new object.
+
+// The keys of the object should be the category names.
+
+// The values should be arrays of the product names belonging to that category.
+
+function groupByCategory(array) {
+  return array.reduce((acc, el) => {
+    return {
+      ...acc,
+      [el.category]: acc[el.category]
+        ? [...acc[el.category], el.name]
+        : [el.category],
+    };
+  }, {});
+}
+
+console.log("Task 2 ", groupByCategory(products));
