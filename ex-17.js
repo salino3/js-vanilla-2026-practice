f20418c21729cfb185f02f3376d401780d0b36f2
Salino3 <flaviobcn012@gmail.com>
2026-04-02 17:17:03 +0200

    save changes

--- ex-17.js
+++ ex-17.js
@@ -34,6 +34,13 @@ const matches = [
     winner: "Djokovic",
     duration: 150,
   },
+  {
+    id: 6,
+    sport: "Tennis",
+    players: ["Djokovic", "Sinner"],
+    winner: "Sinner",
+    duration: 250,
+  },
 ];
 
 // bySport: An object where each key is the sport name.
@@ -44,13 +51,17 @@ const matches = [
 
 // totalPlayTime: The sum of all duration values across all matches.
 
-// playerRecord: An object showing how many matches each player/team participated in (win or lose).
+//* playerRecord: An object showing how many matches each player/team participated in (win or lose).
 
 // Example: { Federer: 1, Nadal: 1, Lakers: 1, ... }
 
-// longestMatch: The id of the match with the highest duration.
+//* longestMatch: The id of the match with the highest duration.
 
 function transformData(matches) {
+  const reducedMatches = matches.reduce((acc, match) => {
+    return [...acc, ...match.players];
+  }, []);
+  console.log("clog5", reducedMatches);
   const groupedTransformData = Object.entries(
     Object.groupBy(matches, (match) => {
       return match.sport;
@@ -58,24 +69,26 @@ function transformData(matches) {
   ).reduce(
     (acc, [key, value], index, array) => {
       let highierTime = 0;
+      let idMatch = 0;
 
       acc.bySport[key.toLowerCase()] = value.map((m) => m.winner);
 
       acc["totalPlayTime"] =
         (acc.totalPlayTime || 0) +
         value.reduce((acc, m) => {
-          console.log("clog1", m.players);
+          console.log("clog2", highierTime < m.duration ? m.id : idMatch);
           highierTime = highierTime < m.duration ? m.duration : highierTime;
+          idMatch = highierTime < m.duration ? m.id : idMatch;
           return (acc += m.duration);
         }, 0);
       console.log("clog1", value);
 
-      acc.longestMatch =
-        acc.longestMatch < highierTime ? highierTime : acc.longestMatch;
-
+      //   acc.longestMatch =
+      //     acc.longestMatch < highierTime ? highierTime : acc.longestMatch;
+      acc.idLongestMatch = idMatch;
       return acc;
     },
-    { bySport: {}, playerRecord: {}, longestMatch: 0 },
+    { bySport: {}, playerRecord: {}, idLongestMatch: 0 },
   );
 
   return groupedTransformData;
