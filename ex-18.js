65da3e7eebc7f12f17c67e15dff106c913f10ffc
Salino3 <flaviobcn012@gmail.com>
2026-04-05 10:52:27 +0200

    save changes

--- ex-18.js
+++ ex-18.js
@@ -1,3 +1,4 @@
+// Recursive functions exercises
 const number = 20;
 
 // The Goal: Write a function countdown(n) that prints numbers from n down
@@ -18,7 +19,7 @@ function countdown(n) {
   return countdown(n);
 }
 
-// console.log("Task 1:", countdown(number));
+console.log("Task 1:", countdown(number));
 
 // better version respecting immutability
 function countdownV2(n) {
@@ -32,7 +33,7 @@ function countdownV2(n) {
   return countdown(n - 1);
 }
 
-// console.log("Task 1 V2:", countdownV2(number));
+console.log("Task 1 V2:", countdownV2(number));
 
 //
 // Write a function sum(arr) that calculates the total of all numbers in an array.
@@ -75,3 +76,48 @@ function isPalindrome(str) {
 }
 
 console.log("Task 3:", isPalindrome("alslA"));
+
+//
+// Write a function flatten(arr) that takes an array containing nested arrays and returns a single "flat" array.
+
+// Example: flatten([1, [2, [3, 4], 5]]) should return [1, 2, 3, 4, 5].
+
+// Hint: Iterate through the elements. If an element is an array, call flatten on it; if it’s a number, push it to your result.
+
+function flatten(arr) {
+  let result = [];
+
+  for (let element of arr) {
+    if (Array.isArray(element)) {
+      result = result.concat(flatten(element));
+    } else {
+      result.push(element);
+    }
+  }
+
+  return result;
+}
+
+console.log(
+  "Task 4:",
+  flatten([1, [2, [3, 4], 5, 6], 7, [8, [9, 10], 11, 12]]),
+);
+
+//
+const flattenV2 = (arr) =>
+  arr.reduce(
+    (acc, val) =>
+      Array.isArray(val) ? acc.concat(flattenV2(val)) : acc.concat(val),
+    [],
+  );
+
+console.log(
+  "Task 4 V2:",
+  flattenV2([1, [2, [3, 4], 5, 6], 7, [8, [9, 10], 11, 12]]),
+);
+
+// JS method
+const nested = [1, [2, [3, 4], 5, 6], 7, [8, [9, 10], 11, 12]];
+
+const totallyFlat = nested.flat(Infinity);
+console.log("flat method", totallyFlat);
