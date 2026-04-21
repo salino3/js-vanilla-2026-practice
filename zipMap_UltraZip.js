7f6f2ef5f4f3ef2202039982e920ef9bce4a128d
Salino3 <flaviobcn012@gmail.com>
2026-04-21 16:18:35 +0200

    save changes

--- zipMap_UltraZip.js
+++ zipMap_UltraZip.js
@@ -1,5 +1,5 @@
 // ---
-console.log("------------------------");
+console.log("-- 2 arrays ------------------------");
 const zipMap = (arr1, arr2, callback) => {
   const maxLength = Math.max(arr1.length, arr2.length);
 
@@ -8,17 +8,20 @@ const zipMap = (arr1, arr2, callback) => {
   });
 };
 
-console.log("------------------------");
+console.log("-- multiple arrays ------------------------");
 
 // This function takes many arrays, then the callback at the end
-const ultraZip = (...args) => {
-  const callback = args.pop(); // The last argument is the function
-  const arrays = args; // The rest are all our arrays
+const ultraZipMap = (...args) => {
+  // 1. Extract the callback
+  const callback = args.slice(-1)[0];
+
+  // 2. Everything else is our data
+  const arrays = args.slice(0, -1);
 
   const maxLength = Math.max(...arrays.map((a) => a.length));
 
   return Array.from({ length: maxLength }).map((_, i) => {
-    // We pass an array of all items at index 'i' to the callback
+    // Pass an array of all items at index 'i' to the callback
     const currentItems = arrays.map((a) => a[i]);
     return callback(...currentItems, i, arrays);
   });
@@ -31,14 +34,14 @@ const prices = [50, 15, 400, 80];
 
 // --- USAGE ---
 console.log(
-  "Test UltraZip function: ",
-  ultraZip(products, quantities, prices, (prod, qty, price, i, allData) => {
+  "Test UltraZipMap function: ",
+  ultraZipMap(products, quantities, prices, (prod, qty, price, i, allData) => {
     // allData[0] is products
     // allData[1] is quantities
     // allData[2] is prices
 
     console.log(
-      `Total items in ${[price]} list: ${allData[i ?? 0].length}`,
+      `Total items in 'allData' list: ${allData?.length ?? 0}`,
       allData,
     );
     return { prod, total: (qty ?? 0) * price };
