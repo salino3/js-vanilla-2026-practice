f2308c933c5feb8e72d731382edb8e33ad746963
Salino3 <flaviobcn012@gmail.com>
2026-04-27 20:02:24 +0200

    done

--- ex-24.js
+++ ex-24.js
@@ -6,7 +6,36 @@
 // If there are no words with repeating letters, return -1.
 
 function letterCount(str) {
-  return "Solve the exercise";
+  const strings = str.split(" ");
+  let counting = 0;
+  let finalResult = "";
+
+  const result = strings.reduce((acc, currentWord) => {
+    let wordMax = 0;
+    for (let i = 0; i < currentWord.length; i++) {
+      let found =
+        currentWord.split("").filter((letter) => letter === currentWord[i])
+          .length ?? 0;
+      if (found > wordMax) {
+        wordMax = found;
+      }
+      acc[currentWord] = wordMax;
+    }
+    return acc;
+  }, {});
+
+  for (value in result) {
+    if (result[value] > counting) {
+      counting = result[value];
+      finalResult = value;
+    }
+  }
+
+  if (counting > 1) {
+    return finalResult;
+  } else {
+    return -1;
+  }
 }
 
 console.log("Task 1:", letterCount("Today is the greatest day ever!"));
