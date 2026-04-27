fef11b7bdedce719fc85d9cc981159af1c7491e6
Salino3 <flaviobcn012@gmail.com>
2026-04-27 18:10:43 +0200

    save changes

---
+++ ex-24.js
@@ -0,0 +1,12 @@
+// Write a function LetterCount(str) that takes a string and returns the first word
+//  with the greatest number of repeated letters.
+
+// If a word has more than one letter that repeats, count the one that repeats the most.
+
+// If there are no words with repeating letters, return -1.
+
+function letterCount(str) {
+  return "Solve the exercise";
+}
+
+console.log("Task 1:", letterCount("Today is the greatest day ever!"));
