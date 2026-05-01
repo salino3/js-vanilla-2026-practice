8b732c4f4f5bce309c158cbcbbbe0e7b1656b9de
Salino3 <flaviobcn012@gmail.com>
2026-05-01 14:09:53 +0200

    add some html

---
+++ project-01/js/app.js
@@ -0,0 +1,10 @@
+// Example: Creating a simple component-like structure
+const Page1 = `
+  <section class="page-content">
+    <h1>Welcome to Page 1</h1>
+    <p>This was injected by JavaScript!</p>
+    
+  </section>
+`;
+
+document.getElementById("app").innerHTML = Page1;
