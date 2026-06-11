bece531f8b9b0a87a94073cd36d6195638461bbb
Salino3 <flaviobcn012@gmail.com>
2026-06-11 11:20:44 +0200

    save changes

---
+++ ex-32.js
@@ -0,0 +1,32 @@
+// Write a function called lengthOfLongestSubstring that takes a string 's' and returns
+// the length of the longest substring that contains no duplicate characters.
+
+//  Rules & Assumptions
+// A "substring" must be contiguous (connected). For example, in "abcabcbb", "abc"
+// is a substring, but "acb" is a subsequence, not a substring.
+
+// The input string can contain letters, digits, symbols, and spaces.
+
+// If the string is empty, the return value should be 0.
+
+function lengthOfLongestSubstring(s) {
+  let charSet = new Set();
+  let left = 0;
+  let maxLength = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    while (charSet.has(s[right])) {
+      console.log(s[left]);
+      charSet.delete(s[left]);
+      left++;
+    }
+
+    charSet.add(s[right]);
+
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
+
+console.log("Task 1:", lengthOfLongestSubstring("abcgangolb"));
