3d03b6b819674fd13640aa931e2995039ef001f8
Salino3 <flaviobcn012@gmail.com>
2026-06-29 16:14:45 +0200

    done

--- ex-33.js
+++ ex-33.js
@@ -5,6 +5,21 @@
 // Strings passed in will consist of only letters and spaces. Spaces will be included
 //  only when more than one word is present.
 
-function spinWords(text) {}
+function spinWords(text) {
+  const words = text
+    .replace("", "")
+    .split(/\s/g)
+    .reduce((acc, word) => {
+      if (word.length > 4) {
+        acc += " " + word.split("").reverse().join("");
+      } else {
+        acc += " " + word;
+      }
+
+      return acc;
+    }, "");
+
+  return words;
+}
 
 console.log("Task 1:", spinWords("Hey fellow warriors"));
