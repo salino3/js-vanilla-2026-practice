87e9dc4afeaaad52f3bfa8ea892580fe15f00272
Salino3 <flaviobcn012@gmail.com>
2026-06-29 16:03:48 +0200

    save changes

---
+++ ex-33.js
@@ -0,0 +1,10 @@
+// The Challenge: "The Word Spinner"
+// Write a function called spinWords that takes in a string of one or more words and
+//  returns the same string, but with all five or more letter words reversed.
+
+// Strings passed in will consist of only letters and spaces. Spaces will be included
+//  only when more than one word is present.
+
+function spinWords(text) {}
+
+console.log("Task 1:", spinWords("Hey fellow warriors"));
