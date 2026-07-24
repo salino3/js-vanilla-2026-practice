c9bb86c9abb0c5e700a7040ff0ae0d531cb2833b
Salino3 <flaviobcn012@gmail.com>
2026-07-24 09:55:37 +0200

    copy exercise

---
+++ example-29/ex-34.js
@@ -0,0 +1,16 @@
+// 🏋️ Exercise: "First Non-Repeating Character"
+// Write a function named firstNonRepeatingChar that takes a string as input and
+// returns the first character that does not repeat consecutively anywhere in the string.
+
+// If every character repeats, return null.
+
+// Case sensitivity matters (e.g., 'a' and 'A' are considered different characters).
+
+function firstNonRepeatingChar(str) {
+  // Your code here
+}
+
+// Tests
+console.log(firstNonRepeatingChar("leetcode")); // Expected: "l"
+console.log(firstNonRepeatingChar("loveleetcode")); // Expected: "v"
+console.log(firstNonRepeatingChar("aabbcc")); // Expected: null
