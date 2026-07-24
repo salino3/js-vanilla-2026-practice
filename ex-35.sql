a7f04f66b8f85007447395b5a6052ab84e51d4cd
Salino3 <flaviobcn012@gmail.com>
2026-07-24 17:25:10 +0200

    save changes

--- ex-35.sql
+++ ex-35.sql
@@ -9,4 +9,27 @@ select first_name, last_name FROM patients Where allergies IS null;
 -- Show first name of patients that start with the letter 'C'
 select first_name from patients where first_name like 'C%';
 
+-- Show first name and last name of patients that weight within the range of 100 to 120 (inclusive)
+select first_name, last_name from patients where weight between 100 AND 120;
+
+-- Update the patients table for the allergies column. If the patient's allergies is null then replace it with 'NKA'
+UPDATE patients SET allergies = 'NKA' WHERE allergies IS null;
+
+-- Show first name and last name concatinated into one column to show their full name.
+SELECT CONCAT ( first_name ,   ' ', last_name ) AS full_name from patients;
+
+-- Show first name, last name, and the full province name of each patient.
+SELECT 
+  first_name,     
+  last_name,       
+  province_name     
+FROM patients 
+JOIN province_names ON province_names.province_id = patients.province_id;
+
+-- Show how many patients have a birth_date with 2010 as the birth year.
+SELECT COUNT(*) AS total_patients
+FROM patients
+WHERE YEAR(birth_date) = 2010;
+
+
 
