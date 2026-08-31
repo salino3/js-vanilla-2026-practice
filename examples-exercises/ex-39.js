302c810bcebdb3938170756ab7550c125b3ef1f5
Salino3 <flaviobcn012@gmail.com>
2026-08-31 15:04:44 +0200

    save changes

--- examples-exercises/ex-39.js
+++ examples-exercises/ex-39.js
@@ -11,7 +11,6 @@ function firstNonRepeatingChar(str) {
   let tempArr = str.split("");
   let start = tempArr[0];
   while (true) {
-    console.log("clog2");
     tempArr.shift();
     if (!tempArr.includes(start)) {
       if (start) {
@@ -61,7 +60,30 @@ console.log("Task 2", isAnagram("Ónnroegn  rgeSwino", "ónn roegnrgewinos"));
 // The function should be case-sensitive ('a' and 'A' are different characters).
 
 function compressString(str) {
-  // Your code here
+  const result = [];
+
+  const reducedStr = str
+    .split("")
+
+    .reduce((acc, word) => {
+      console.log("word", word);
+
+      if (acc.length === 0) {
+        acc.push([word]);
+      } else {
+        console.log("clog3", acc);
+
+        if (acc[acc.length - 1][0] && acc[acc.length - 1][0] === word) {
+          acc[acc.length - 1].push(word);
+        } else {
+          acc.push([word]);
+        }
+      }
+
+      return acc;
+    }, []);
+
+  return reducedStr;
 }
 
-console.log("Task 3", compressString("aabcccccaaa"));
+console.log("Task 3", compressString("aaAbcccccaaa"));
