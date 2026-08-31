b015d8f128bf408a95b6881c5e55a5316f632afe
Salino3 <flaviobcn012@gmail.com>
2026-08-31 12:53:56 +0200

    save changes

--- examples-exercises/ex-39.js
+++ examples-exercises/ex-39.js
@@ -5,6 +5,25 @@
 
 // The function should be case-sensitive (e.g., 'a' and 'A' are different characters).
 
-function firstNonRepeatingChar(str) {}
+function firstNonRepeatingChar(str) {
+  if (!str) return null;
+
+  let tempArr = str.split("");
+  let start = tempArr[0];
+  while (true) {
+    console.log("clog2");
+    tempArr.shift();
+    if (!tempArr.includes(start)) {
+      if (start) {
+        return start;
+      } else {
+        return null;
+      }
+    } else {
+      tempArr = tempArr.filter((char) => char !== start);
+      start = tempArr[0];
+    }
+  }
+}
 
 console.log("Task 1", firstNonRepeatingChar("loveleetcode"));
