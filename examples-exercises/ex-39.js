f7bb2deea671a8eedb05254b5b40483f590f4d29
Salino3 <flaviobcn012@gmail.com>
2026-08-31 12:22:11 +0200

    add new exercise

---
+++ examples-exercises/ex-39.js
@@ -0,0 +1,10 @@
+// Write a function named firstNonRepeatingChar that takes a string as an input and returns
+// the first character that does not repeat anywhere in the string.
+
+// If every character repeats, return null.
+
+// The function should be case-sensitive (e.g., 'a' and 'A' are different characters).
+
+function firstNonRepeatingChar(str) {}
+
+console.log("Task 1", firstNonRepeatingChar("loveleetcode"));
