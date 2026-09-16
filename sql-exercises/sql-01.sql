d3c494e112a684851e6e403b6b1f485a8c83cdd5
Salino3 <flaviobcn012@gmail.com>
2026-09-16 11:13:13 +0200

    some exercises

--- sql-exercises/sql-01.sql
+++ sql-exercises/sql-01.sql
@@ -1,1 +1,21 @@
 -- https://www.sql-practice.com/
+
+SELECT contact_name, address, city from customers where country NOT IN ('Germany', 'Mexico', 'Spain');
+
+-- Show the employee_id, order_id, customer_id, required_date, shipped_date from all orders shipped later than the required date
+SELECT employee_id, order_id, customer_id, required_date, shipped_date
+FROM orders
+WHERE shipped_date > required_date;
+
+-- Show all the even numbered Order_id from the orders table
+ select   order_id from orders where order_id % 2 = 0 ;
+-- Or
+SELECT order_id
+FROM orders
+WHERE mod(order_id,2) = 0;
+
+
+
+
+
+
