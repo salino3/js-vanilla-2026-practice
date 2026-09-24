65bcd040a98e9256dbc36e5215d64cdc693756b4
Salino3 <flaviobcn012@gmail.com>
2026-09-24 15:31:16 +0200

    save changes

---
+++ example-exercise-03/App.tsx
@@ -0,0 +1,13 @@
+import { UserList } from "./components/user-list.tsx";
+import { ShopApp } from "./components/dashboard-cart/dashboard-cart.component.tsx";
+
+function App() {
+  return (
+    <div>
+      {/* <UserList /> */}
+      <ShopApp />
+    </div>
+  );
+}
+
+export default App;
